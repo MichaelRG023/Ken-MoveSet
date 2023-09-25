ab5b8433f3d3c11f7d76de4966f565ae50b64bbf
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2023-09-25 17:31:49 -0400

    created log in system which has username,,password and Avatar logo for users to log into, the avatar logo changes based on what is sent towards the url

---
+++ src/signUp.js
@@ -0,0 +1,77 @@
+
+import React,{useState} from "react"
+
+
+function SignUp(){
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        avatar: "",
+    })
+   
+    
+   
+    function handleSubmit(e){
+        e.preventDefault();
+        
+
+    }
+
+    function handleChange(e){
+      const key = e.target.id
+      setFormData({
+        ...formData,
+        [key]: e.target.value
+      })
+    }
+    console.log(formData)
+
+
+    
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <h1>Create an Account</h1>
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            id="username"
+            value={formData.username}
+            onChange={handleChange}
+            />
+
+            <label htmlFor="password">password</label>
+            <input
+            type="text"
+            id= "password"
+            value={formData.password}
+            onChange={handleChange}
+            />
+
+
+
+            <label htmlFor="type">Avatar Image</label>
+            <input
+            type="text"
+            id="avatar"
+            value={formData.avatar}
+            onChange={handleChange}
+            />
+            <img
+            src={formData.avatar ||
+            "https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.png"
+            }
+            alt="Avatar preview"
+            />
+
+
+            <input type="submit" value="Sign Up"/>
+         </form>
+    
+)
+}
+
+
+
+
+export default  SignUp;
