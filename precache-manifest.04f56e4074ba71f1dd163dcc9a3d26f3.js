6611f3df96c80153fd32d4e70b5e64aed014e4ca
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2023-10-31 17:29:47 -0400

    Updates

---
+++ precache-manifest.04f56e4074ba71f1dd163dcc9a3d26f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fff8ec668ef8891e9bc6b7902d97633",
+    "url": "/Ken-MoveSet/index.html"
+  },
+  {
+    "revision": "275abc5867c4c59ecab6",
+    "url": "/Ken-MoveSet/static/css/main.012867d4.chunk.css"
+  },
+  {
+    "revision": "e85c66b03a5c509bbad0",
+    "url": "/Ken-MoveSet/static/js/2.4b732f1a.chunk.js"
+  },
+  {
+    "revision": "275abc5867c4c59ecab6",
+    "url": "/Ken-MoveSet/static/js/main.ade3eb39.chunk.js"
+  },
+  {
+    "revision": "f0125384197682814661",
+    "url": "/Ken-MoveSet/static/js/runtime~main.4872af25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eaf5c38944e9b4f80ec5e9a877a40f04.js"
+  "/Ken-MoveSet/precache-manifest.04f56e4074ba71f1dd163dcc9a3d26f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ken-MoveSet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
