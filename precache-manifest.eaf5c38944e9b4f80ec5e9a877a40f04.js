51b421f61de41b486a85aa81e1b69d2ab54e6371
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2023-10-31 17:08:42 -0400

    Updates

---
+++ precache-manifest.eaf5c38944e9b4f80ec5e9a877a40f04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0295d20051395c39505ee03fbfd34bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a0c46c118e0929e156",
+    "url": "/static/css/main.4bba4f63.chunk.css"
+  },
+  {
+    "revision": "461fe425a7a58b0cb9a9",
+    "url": "/static/js/2.47d9e2c2.chunk.js"
+  },
+  {
+    "revision": "26a0c46c118e0929e156",
+    "url": "/static/js/main.f7f46439.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
